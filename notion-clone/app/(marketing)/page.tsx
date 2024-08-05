29ab79feecff20a41dbdce5e3851c5875885d9db
qh2244 <qifenghuang@outlook.com>
2024-08-05 16:59:15 -0700

    Notion_Clone Project started! Working on landingpage now

---
+++ notion-clone/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
