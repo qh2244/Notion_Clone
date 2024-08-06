2f6c8158ba30acdf803e4aa935cbd4f6c3b22239
qh2244 <qifenghuang@outlook.com>
2024-08-05 18:50:37 -0700

    Added convex dev and clerk user auth

--- notion-clone/app/(marketing)/page.tsx
+++ notion-clone/app/(marketing)/page.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Footer } from "./_components/footer";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button variant="destructive">Click me</Button>
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
     </div>
   );
-}
+};
+
+export default MarketingPage;
---
+++ notion-clone/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://prepared-elephant-99.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
